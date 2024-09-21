ec201a729b168aeac6de6c429f8064b7df6281f7
Haley <haleyh103@hotmail.com>
2024-09-21 19:20:42 -0400

    bruh

--- frontend/src/contact.js
+++ frontend/src/contact.js
@@ -11,11 +11,15 @@ function Contact() {
 
 <section className="header-section text-center">
         <div className="container">
-          <h1 className="main-title">Contact</h1>
+          <h1 className="pagetitle">Contact</h1>
           <p className="subtitle">Connecting People, Ideas, and Resources for Financial Growth</p>
         </div>
       </section>
+
+<h1 className="main-title"> Our team</h1>
+
     </>
+
   );
 }
 
